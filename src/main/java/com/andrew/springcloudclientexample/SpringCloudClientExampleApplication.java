cbb33273b0fe09233ffd2a50e951b608f990749f
MrDexII <free.andrew.a@gmail.com>
2021-05-25 20:14:10 +0200

    initial commit

---
+++ src/main/java/com/andrew/springcloudclientexample/SpringCloudClientExampleApplication.java
@@ -0,0 +1,13 @@
+package com.andrew.springcloudclientexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudClientExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrew/springcloudclientexample/SpringCloudClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.springcloudclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
