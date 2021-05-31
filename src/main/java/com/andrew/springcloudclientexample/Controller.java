ad2abb7f1dba2a7836099c6bc513eca3636c0477
MrDexII <free.andrew.a@gmail.com>
2021-05-31 14:56:11 +0200

    config server

--- src/main/java/com/andrew/springcloudclientexample/Controller.java
+++ src/main/java/com/andrew/springcloudclientexample/Controller.java
@@ -1,13 +1,19 @@
 package com.andrew.springcloudclientexample;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class Controller {
 
-    @GetMapping
+    @Value("${msg:Can not found message server not responding}")
+    private String msg;
+
+    @GetMapping("/hello")
     public String hello() {
-        return "Hello";
+        return this.msg;
     }
 }
