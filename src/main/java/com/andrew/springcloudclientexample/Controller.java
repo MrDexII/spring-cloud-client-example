63f822294aade950b7e00459a6593778046485a1
MrDexII <free.andrew.a@gmail.com>
2021-06-17 16:27:12 +0200

    config change

--- src/main/java/com/andrew/springcloudclientexample/Controller.java
+++ src/main/java/com/andrew/springcloudclientexample/Controller.java
@@ -7,8 +7,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-
 @RestController
 @RefreshScope
 public class Controller {
--- src/main/java/com/andrew/springcloudclientexample/SpringCloudClientExampleApplication.java
+++ src/main/java/com/andrew/springcloudclientexample/SpringCloudClientExampleApplication.java
@@ -2,8 +2,10 @@ package com.andrew.springcloudclientexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringCloudClientExampleApplication {
 
 	public static void main(String[] args) {
